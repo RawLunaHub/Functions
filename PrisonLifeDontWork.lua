281f1d1640708dff056a503dba467745d8a11a24
RawLunaHub <83168446+RawLunaHub@users.noreply.github.com>
2022-11-23 20:16:05 +0300

    Update PrisonLifeDontWork.lua

--- PrisonLifeDontWork.lua
+++ PrisonLifeDontWork.lua
@@ -2,19 +2,10 @@
 
 if Weapon == "M9" then
     local data = game:GetService("Players").LocalPlayer.Character.M9.GunStates
-    local changeData = require(data)
-else
-    print("not M9")
 end
 if Weapon == "AK-47" then
     local data = game:GetService("Players").LocalPlayer.Character["AK-47"].GunStates
-    local changeData = require(data)
-else
-    print("not AK-47")
 end
 if Weapon == "870" then
     local data = game:GetService("Players").LocalPlayer.Character["Remington 870"].GunStates
-    local changeData = require(data)
-else
-    print("not Remington 870")
 end
