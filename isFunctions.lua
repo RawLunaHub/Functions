b1777ade49fd4ac2ecc5d9019aaf10f62761f6d7
RawLunaHub <83168446+RawLunaHub@users.noreply.github.com>
2022-11-23 17:09:17 +0300

    Update isFunctions.lua

--- isFunctions.lua
+++ isFunctions.lua
@@ -1,10 +1,4 @@
-function Status()
-    if Pl.Status.toollsEquipped == true then
-        a = true
-    else
-        a = false
-    end
-end
+
 function isBadGuard()
     if Pl.Status.isBadGuard == true then
         Pl.Status.isBadGuard = false
