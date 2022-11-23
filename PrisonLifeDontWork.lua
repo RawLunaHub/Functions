81b7c3806454b84301e87a89745743145dce65ff
RawLunaHub <83168446+RawLunaHub@users.noreply.github.com>
2022-11-23 17:03:47 +0300

    ez
    ez

--- PrisonLifeDontWork.lua
+++ PrisonLifeDontWork.lua
@@ -9,15 +9,6 @@ local t = "https://"
 loadstring(game:HttpGet(t.."raw.githubusercontent.com/RawLunaHub/Functions/main/isFunctions.lua"))()
 isBadGuard()
 Status()
-if Item == rem then
-    local Item = "Remington 870"
-    if a == true then
-        attribute = Pl.Backpack[Item].GunStates
-    else
-        attribute = Pl.Character[Item].GunStates
-    end
-else
-    print("ignored")
 end
 if a == true then
     attribute = Pl.Backpack["..Item.."].GunStates
@@ -27,3 +18,4 @@ end
 local isModule = Pl.attribute
 local Data = isModule
 local DataChange = require(Data)
+print("[TEST]")
