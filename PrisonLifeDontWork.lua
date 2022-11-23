d073e81764a7eaa6c6de85b71068c85b1cf8f47d
RawLunaHub <83168446+RawLunaHub@users.noreply.github.com>
2022-11-23 20:07:20 +0300

    Data Restore

--- PrisonLifeDontWork.lua
+++ PrisonLifeDontWork.lua
@@ -1,28 +1,20 @@
-local Pl = game.Players.LocalPlayer
---[[
-for i, object in pairs(Pl.Character:GetDescendants()) do
-    if object:IsA("Tool") then
-        local Item = object.Name
-        print(Item)
-    end
-end
---]]
---[[
-function BadGuard()
-    if Pl.Status.isBadGuard == true then
-        Pl.Status.isBadGuard = false
-    else
-        print("ignored")
-    end
-end
-BadGuard()
---]]
-if Pl.Character.Item.Equipped == true then
-    print("Module Check..")
-    local isModule = Pl.Character.Item.GunStates
-    local DataChange = require(isModule)
+--local Pl = game.Players.LocalPlayer
+
+if Weapon == "M9" then
+    local data = game:GetService("Players").LocalPlayer.Character.M9.GunStates
+    local changeData = require(data)
 else
-    print("u didnt equip ur weapon to change values in module")
+    print("not M9")
 end
-print(a)
-print("[TEST]")
+if Weapon == "AK-47" then
+    local data = game:GetService("Players").LocalPlayer.Character["AK-47"].GunStates
+    local changeData = require(data)
+else
+    print("not AK-47")
+end
+if Weapon == "870" then
+    local data = game:GetService("Players").LocalPlayer.Character["Remington 870"].GunStates
+    local changeData = require(data)
+else
+    print("not Remington 870")
+end
