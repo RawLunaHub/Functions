8d23eef27be253188401c318fa605f708a376dc7
RawLunaHub <83168446+RawLunaHub@users.noreply.github.com>
2022-11-23 16:43:29 +0300

    DontWork
    work = false

---
+++ PrisonLifeDontWork.lua
@@ -0,0 +1,27 @@
+a = false
+local t = "https://"
+if t == "https://" then
+    loadstring(game:HttpGet(t.."raw.githubusercontent.com/RawLunaHub/Functions/main/isFunctions.lua"))()
+else
+    warn("[DEBUG] Error: t not a https:// ")
+end
+isBadGuard()
+Status()
+if Item == rem then
+    local Item = "Remington 870"
+    if a == true then
+        attribute = Pl.Backpack[Item].GunStates
+    else
+        attribute = Pl.Character[Item].GunStates
+    end
+else
+    print("ignored")
+end
+if a == true then
+    attribute = Pl.Backpack["..Item.."].GunStates
+else
+    attribute = Pl.Character["..Item.."].GunStates
+end
+local isModule = Pl.attribute
+local Data = isModule
+requireData(i,v)
